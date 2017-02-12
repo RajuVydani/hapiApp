3b3f405eeb1ce529432f24cfb2d60fafa192e0e7
Raju <raju.vydani@gmail.com>
2017-02-12 15:26:10 +0530

    first commit

---
+++ routes.js
@@ -0,0 +1,33 @@
+//routes can be passed as object or array of objects..
+
+module.exports = [
+
+	{
+		method : 'GET',
+		path : '/first',
+		handler : function(request, reply){
+			reply('first api...');
+		}
+	},
+	{
+		method : 'GET',
+		path : '/second',
+		handler : function(request, reply) {
+			reply('second api...');
+		}
+	},
+	{
+		method : 'GET',
+		path : '/json',
+		handler : function(request, reply) {
+			var data = {
+				name : 'raju',
+				city : 'Hyderabad',
+				occupation : 'employee',
+				state : 'Andhra Pradesh'
+			};
+			reply(data);
+		}
+	}
+
+];
---
+++ server.js
@@ -0,0 +1,23 @@
+const hapi = require('hapi');
+const routes = require('./routes');
+
+const server = new hapi.Server();
+
+server.connection({
+	host: 'localhost',
+	port: '8080'
+});
+
+/*server.route({
+	method : 'GET',
+	path : '/test',
+	handler : function(request, reply) {
+		reply('hello world !');
+	}
+});*/
+
+server.route(routes);
+
+server.start(function(){
+	console.log('hapi Server Started...');
+});
