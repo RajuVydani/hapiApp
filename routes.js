9e0e81900881d94c99a1c01dfcc2e56cf059a6be
RajuVydani <raju.vydani@gmail.com>
2017-02-18 12:53:26 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ module.exports = [
 		path : '/json',
 		handler : function(request, reply) {
 			var data = {
-				name : 'Prasad',
+				name : 'Prasad1',
 				city : 'Hyderabad',
 				occupation : 'employee',
 				state : 'Andhra Pradesh'
