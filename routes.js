2fa025fd0ee5ef27493353a90f74482bf56ae395
Raju <raju.vydani@gmail.com>
2017-02-19 01:10:18 +0530

    conflicts

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ module.exports = [
 		path : '/json',
 		handler : function(request, reply) {
 			var data = {
-				name : 'raju3',
+				name : 'durga',
 				city : 'Hyderabad',
 				occupation : 'employee',
 				state : 'Andhra Pradesh'
