40523eb2a253556708d02980d9d3ca4686ef19fe
Raju <raju.vydani@gmail.com>
2017-02-18 15:18:32 +0530

    prasad6

--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ module.exports = [
 		path : '/json',
 		handler : function(request, reply) {
 			var data = {
-				name : 'Prasad6',
+				name : 'raju',
 				city : 'Hyderabad',
 				occupation : 'employee',
 				state : 'Andhra Pradesh'
